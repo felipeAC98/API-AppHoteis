663139da31fbe5b81574b1c1cc3ffad00878cab7
FelipeAC98 <felipe.alvcor@gmail.com>
2021-11-26 20:36:50 -0300

    Criacao metodo para obter unico hotel pelo ID

--- primeiroRestAPI.py
+++ primeiroRestAPI.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from resources.hotel import Hoteis
+from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
 api = Api(app) 
 
+#o primeiro parametro eh referente a classe que sera invocada
 api.add_resource(Hoteis,'/hoteis') #adicionando o recurso dentro da URI /hoteis
+api.add_resource(Hotel,'/hoteis/<string:hotel_id>') #adicionando o recurso dentro da URI /hoteis
 
 if __name__=='__main__':
     app.run(debug=True)
--- resources/hotel.py
+++ resources/hotel.py
@@ -23,9 +23,55 @@ hoteis = [
     'cidade':'Bady Bassitt'
 }
 ]
+
+#mesma lista de cima porem em forma de dicionario para ter uma busca O1 quando for relacionada ao ID do hotel
+hoteis_O1 = {
+    'hotel_id':{
+        'alpha':{
+            'hotel_id':'alpha',
+            'nome':'Alpha Hotel',
+            'estrelas':4.3,
+            'diaria':420,
+            'cidade':'Sao Jose do Rio Preto'
+        },
+        'bravo':{
+            'hotel_id':'bravo',
+            'nome':'bravo Hotel',
+            'estrelas':4.8,
+            'diaria':730,
+            'cidade':'Sao Paulo'
+         },
+        'charlie':{
+            'hotel_id':'charlie',
+            'nome':'charlie Hotel',
+            'estrelas':3.3,
+            'diaria':50,
+            'cidade':'Bady Bassitt'
+         },        
+    }
+}
+
 #herdando a classe resource
 class Hoteis(Resource):
 
     #todo resource possui get/post/...
     def get(self):
-        return {'hoteis': hoteis}#retornando um dict que ira ser convertido para json
+        return {'hoteis': hoteis_O1}#retornando um dict que ira ser convertido para json
+
+class Hotel(Resource):
+
+    def get(self, hotel_id):
+        try:
+            return hoteis_O1['hotel_id'][hotel_id]
+        except:
+            return {'message':'Hotel id not found'},404 #padrao para retorno de erros
+        pass
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass    #para nao precisar implementar o codigo ainda
