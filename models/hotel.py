262bb55ca98fbb3c9156a31e39d77fdad521152d
FelipeAC98 <felipe.alvcor@gmail.com>
2021-11-27 14:56:27 -0300

    Criando banco a partir do SQL alchemy
    OBS: a pasta models ja deveria ter sido mergeada anteriormente

--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,14 @@
-class HotelModel(): #herdando o modelo de banco
+from sql_alchemy import banco
+
+class HotelModel(banco.Model): #herdando o modelo de banco
+
+    __tablename__ = 'hoteis' #necessario para nomear a tabela no SQL_alchemy
+
+    hotel_id=banco.Column(banco.String, primary_key=True) #definindo que a variavel hotel_id sera uma string e do tipo [prymary key]
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1)) #numeros de casa depois da virgula
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
 
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id=hotel_id
--- primeiroRestAPI.py
+++ primeiroRestAPI.py
@@ -2,12 +2,20 @@ from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
-app = Flask(__name__)
+app = Flask(__name__)                                       #o app do tipo flask
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///banco.db' #para criar na raiz do diretorio um banco do tipo sqlite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False         #para evitar o aviso de alteracao
 api = Api(app) 
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all() #criando o banco e tabelas antes da primeira requisicao
 
 #o primeiro parametro eh referente a classe que sera invocada
 api.add_resource(Hoteis,'/hoteis') #adicionando o recurso dentro da URI /hoteis
 api.add_resource(Hotel,'/hoteis/<string:hotel_id>') #adicionando o recurso dentro da URI /hoteis
 
 if __name__=='__main__':
+    from sql_alchemy import banco 
+    banco.init_app(app) 
     app.run(debug=True)
