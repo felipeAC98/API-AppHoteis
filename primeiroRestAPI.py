1860f80d2802ddf5275cb1291e223f112343db12
FelipeAC98 <felipe.alvcor@gmail.com>
2021-11-28 13:04:56 -0300

    Implementando login de usuario - JWT

--- primeiroRestAPI.py
+++ primeiroRestAPI.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User, UserRegister
+from resources.usuario import User, UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)                                       #o app do tipo flask
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///banco.db' #para criar na raiz do diretorio um banco do tipo sqlite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False         #para evitar o aviso de alteracao
+app.config['JWT_SECRET_KEY']='213knfsldr13'                 #definindo a chave de criptografia
 api = Api(app) 
+jwt = JWTManager(app) #para tratar das questoes de autenticacao do app
 
 @app.before_first_request
 def cria_banco():
@@ -16,8 +19,9 @@ def cria_banco():
 api.add_resource(Hoteis,'/hoteis') #adicionando o recurso dentro da URI /hoteis
 api.add_resource(Hotel,'/hoteis/<string:hotel_id>') #adicionando o recurso dentro da URI /hoteis
 
-api.add_resource(User,'/usuarios/<int:user_id>') #adicionando o recurso dentro da URI /hoteis
-api.add_resource(UserRegister,'/cadastro') #adicionando o recurso dentro da URI /hoteis
+api.add_resource(User,'/usuarios/<int:user_id>') #adicionando o recurso dentro da URI /usuarios
+api.add_resource(UserRegister,'/cadastro') #adicionando o recurso dentro da URI /cadastro
+api.add_resource(UserLogin,'/login') #adicionando o recurso dentro da URI /login
 
 if __name__=='__main__':
     from sql_alchemy import banco 
--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,12 @@
 from flask_restful import Resource , reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
+
+atributos=reqparse.RequestParser()
+atributos.add_argument('login',type=str,required=True, help="The field 'login' cannot be left blank")
+atributos.add_argument('senha',type=str,required=True, help="The field 'senha' cannot be left blank")
+
 
 class User(Resource):
     #/usuarios/{user_id}
@@ -28,10 +35,6 @@ class User(Resource):
 class UserRegister(Resource):
 
     def post(self):
-        atributos=reqparse.RequestParser()
-        atributos.add_argument('login',type=str,required=True, help="The field 'login' cannot be left blank")
-        atributos.add_argument('senha',type=str,required=True, help="The field 'senha' cannot be left blank")
-
         dados=atributos.parse_args()    
 
         #Verificando se o usuario ja existe
@@ -41,3 +44,18 @@ class UserRegister(Resource):
         user=UserModel(**dados)
         user.save_user()
         return {"message":'User created successfully'} ,201
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados=atributos.parse_args()  
+
+        user=UserModel.find_by_login(dados['login'])
+
+        #as funcoes abaixo sao provindas do PyJWT e do proprio python
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso= create_access_token(identity=user.user_id)
+            return{'access_token':token_de_acesso},200
+
+        return {'message':'The username or password is incorrect.'},401
