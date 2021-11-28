bd0935fc2c606c06cebfac9bac7a1d25c7f227f5
FelipeAC98 <felipe.alvcor@gmail.com>
2021-11-28 12:01:43 -0300

    Atualizacao metodo DELETE - sqlite e refatoracao do codigo

--- resources/hotel.py
+++ resources/hotel.py
@@ -57,14 +57,16 @@ class Hoteis(Resource):
 
     #todo resource possui get/post/...
     def get(self):
-        return {'hoteis': hoteis_O1}#retornando um dict que ira ser convertido para json
+        #cada retorno da query eh um objeto do tipo HotelModel
+        #SELECT * FROM HOTEIS
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}#retornando um dict que ira ser convertido para json
 
 class Hotel(Resource):
     #atributos de um hotel (um pouco estranho ver dessa forma mas faz sentido) (eles so sao chamados mesmo quando chamamos o parse_args)
     argumentos= reqparse.RequestParser()
     #somente os argumentos definidos abaixo sao aceitos no post
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
+    argumentos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be left blank")
+    argumentos.add_argument('estrelas', type=float, required=True, help="The field 'estrelas' cannot be left blank")
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
@@ -94,8 +96,11 @@ class Hotel(Resource):
         #passando os kargs como argumento (dadosRecebidos eh um dict) da classe HotelModel e entao instanciando um objeto da classe hotelModel
         novo_hotel=HotelModel(hotel_id, **dadosRecebidos)
 
-        #chamando a funcao save_hotel
-        novo_hotel.save_hotel()
+        try:
+            #chamando a funcao save_hotel
+            novo_hotel.save_hotel()
+        except:
+            return {'message':'An internal error ocurred trying to save hotel'}, 500 #500 eh internal server error
         return novo_hotel.json()
      
     def put(self,hotel_id):
@@ -107,17 +112,28 @@ class Hotel(Resource):
         #caso ohotel ja exista vamos somente atualizar os dados dele
         if hotel_existente :
             hotel_existente.update_hotel(**dadosRecebidos)
-            hotel_existente.save_hotel()
+            hotel_existente.save_hotel() #salvando a alteracao feita no objeto para o banco
             return hotel_existente.json(), 200
 
         else:
             novo_hotel =HotelModel(hotel_id, **dadosRecebidos)
+        try:
+            #chamando a funcao save_hotel
             novo_hotel.save_hotel()
-            return novo_hotel.json(), 201 #novo codigo para indicar que o hotel foi criado
+        except:
+            return {'message':'An internal error ocurred trying to save hotel'}, 500 #500 eh internal server error
+        return novo_hotel.json(), 201 #novo codigo para indicar que o hotel foi criado
 
     def delete(self,hotel_id):
-        global hoteis
-        hoteis=[hotel for hotel in hoteis if hotel['hotel_id'] !=hotel_id]
-        return {'message':'Hotel deleted'}, 200 #novo codigo para indicar que o hotel foi criado
+
+        hotel_existente=HotelModel.find_hotel(hotel_id)
+        if hotel_existente :
+            try:
+                hotel_existente.delete_hotel()
+            except:
+                 return {'message':'An error ocurred trying to delete'}, 500
+            return {'message':'Hotel deleted'}, 200 #novo codigo para indicar que o hotel foi criado
+        
+        return {'message':'Hotel ID was not found'}, 400
         pass    #para nao precisar implementar o codigo ainda
     
